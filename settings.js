e0458df0f4c88f387d2293bbe25431bd0a017bdd
AliSyarif <alisyarif675@gmail.com>
2023-08-15 17:30:12 +0700

    cheemsbot10

---
+++ settings.js
@@ -0,0 +1,73 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
+global.antispam = false //auto kick spammer (true to on, false to off)
+
+
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['628991341381'] //ur owner number
+global.ownernomer = "628991341381" //ur owner number2
+global.ownername = "Ali" //ur owner name
+global.ytname = "x gallade" //ur yt chanel name
+global.socialm = "Instagram: @Alisya_675" //ur github or insta name
+global.location = "Indonesia, Jawa Timur, Surabaya" //ur location
+
+//new
+global.botname = "GalladeBotz"
+global.ownernumber = '628991341381'
+global.ownername = 'Ali'
+global.ownerNumber = ["628991341381@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@DGXeon"
+global.websitex = "https://youtu.be/L_SIk59QeAU"
+global.wagc = "https://chat.whatsapp.com/HGH5Tq4w69eLOMhGo4vVQ8"
+global.themeemoji = '🪀'
+global.wm = "Gallade Bot Inc."
+global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.packname = "Sticker By"
+global.author = "GalladeBot"
+global.creator = "628991341381@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Done', 
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
